48f5513510635ffb5ae02a4903c41cf4425302d5
inespoernomo <ines@traceable.ai>
2020-07-23 09:50:40 -0700

    Update naming

--- gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/config/LogConfig.java
+++ gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/config/LogConfig.java
@@ -3,8 +3,11 @@ package org.hypertrace.gateway.service.entity.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+/**
+ * Configuration for Entity Service logging, outside the log4j configuration
+ */
 public class LogConfig {
-  private static final String LOG_CONFIG = "log.config";
+  private static final String LOG_CONFIG = "entity.service.log.config";
   private static final String MAX_QUERY_IN_MILLIS = "query.threshold.millis";
   private static final long DEFAULT_MAX_QUERY_IN_MILLIS = 2500L;
   private final long queryThresholdInMillis;
