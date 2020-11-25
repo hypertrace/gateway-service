765e668f9e1c7d38525aa7e043d28f40c5673e5d
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2020-11-25 14:30:53 +0530

    updates release workflow and gradle plugin

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   java
   application
-  id("org.hypertrace.docker-java-application-plugin")
-  id("org.hypertrace.docker-publish-plugin")
+  id("org.hypertrace.docker-java-application-plugin") version "0.8.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.8.0"
 }
 
 dependencies {
@@ -37,4 +37,12 @@ application {
 // Config for gw run to be able to run this locally. Just execute gw run here on Intellij or on the console.
 tasks.run<JavaExec> {
   jvmArgs = listOf("-Dbootstrap.config.uri=file:${projectDir}/src/main/resources/configs", "-Dservice.name=${project.name}")
+}
+
+hypertraceDocker {
+  defaultImage {
+    javaApplication {
+      port.set(50072)
+    }
+  }
 }
