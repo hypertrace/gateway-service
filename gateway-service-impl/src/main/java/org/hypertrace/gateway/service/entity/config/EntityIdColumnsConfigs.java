85238e70b694a9810ca0786b8d764ca1d2f70562
Tim Mwangi <tim@traceable.ai>
2020-12-06 11:50:15 -0800

    use immutable map

--- gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/config/EntityIdColumnsConfigs.java
+++ gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/config/EntityIdColumnsConfigs.java
@@ -1,7 +1,7 @@
 package org.hypertrace.gateway.service.entity.config;
 
+import com.google.common.collect.ImmutableMap;
 import com.typesafe.config.Config;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -26,15 +26,14 @@ public class EntityIdColumnsConfigs {
   }
 
   private static EntityIdColumnsConfigs fromConfigList(List<? extends Config> configs) {
-    Map<String, String> scopeToKeyMap = new HashMap<>();
-    EntityIdColumnsConfigs entityIdColumnsConfigs = new EntityIdColumnsConfigs(scopeToKeyMap);
+    ImmutableMap.Builder<String, String> scopeToKeyMapBuilder = ImmutableMap.builder();
     for (Config scopeConfig : configs) {
       String scope = scopeConfig.getString(SCOPE);
       String key = scopeConfig.getString(KEY);
-      scopeToKeyMap.put(scope, key);
+      scopeToKeyMapBuilder.put(scope, key);
     }
 
-    return entityIdColumnsConfigs;
+    return new EntityIdColumnsConfigs(scopeToKeyMapBuilder.build());
   }
 
   public EntityIdColumnsConfigs(Map<String, String> scopeToKeyMap) {
