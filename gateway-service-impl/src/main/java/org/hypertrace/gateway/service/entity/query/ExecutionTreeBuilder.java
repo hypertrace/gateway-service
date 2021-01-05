7eedad746df2bf2eb11abdb425f817a35c6f9de0
SJ <shubham@traceable.ai>
2021-01-05 11:48:51 +0530

    revert back entity id equals filter

--- gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/query/ExecutionTreeBuilder.java
+++ gateway-service-impl/src/main/java/org/hypertrace/gateway/service/entity/query/ExecutionTreeBuilder.java
@@ -154,6 +154,15 @@ public class ExecutionTreeBuilder {
     EntitiesRequest entitiesRequest = executionContext.getEntitiesRequest();
     QueryNode rootNode = createQsDataFetcherNodeWithPagination(entitiesRequest);
     executionContext.setSortAndPaginationNodeAdded(true);
+
+    // If the request has an EntityId EQ filter then there's no need for the 2nd request to get the
+    // total entities. So no need to set the TotalFetcherNode
+    if (ExecutionTreeUtils.hasEntityIdEqualsFilter(executionContext)) {
+      executionContext.setTotal(1);
+    } else {
+      rootNode = new TotalFetcherNode(rootNode, QS.name());
+    }
+
     return rootNode;
   }
 
