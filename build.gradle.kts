e0d8896728d661327d1215bbed9ab6668c2f47ed
Ravi Singal <ravi.singal@traceable.ai>
2021-04-08 22:28:05 +0530

    publish artifacts to hypertrace artifactory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-  id("org.hypertrace.repository-plugin") version "0.1.2"
-  id("org.hypertrace.ci-utils-plugin") version "0.2.0"
-  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
-  id("org.hypertrace.jacoco-report-plugin") version "0.1.0" apply false
-  id("org.hypertrace.docker-java-application-plugin") version "0.8.1" apply false
-  id("org.hypertrace.docker-publish-plugin") version "0.8.1" apply false
-  id("org.hypertrace.code-style-plugin") version "1.0.2" apply false
+  id("org.hypertrace.repository-plugin") version "0.4.0"
+  id("org.hypertrace.ci-utils-plugin") version "0.3.0"
+  id("org.hypertrace.publish-plugin") version "1.0.2" apply false
+  id("org.hypertrace.jacoco-report-plugin") version "0.2.0" apply false
+  id("org.hypertrace.docker-java-application-plugin") version "0.9.0" apply false
+  id("org.hypertrace.docker-publish-plugin") version "0.9.0" apply false
+  id("org.hypertrace.code-style-plugin") version "1.1.0" apply false
 }
 
 subprojects {
