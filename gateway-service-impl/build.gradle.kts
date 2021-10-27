0a6a4c066fe03744b1be84dc8b3f1728d8d82bb2
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-10-27 13:44:27 -0400

    fix: align grpc versions (#111)
    * fix: align grpc versions
    
    * fix: add missing close quote

--- gateway-service-impl/build.gradle.kts
+++ gateway-service-impl/build.gradle.kts
@@ -34,5 +34,5 @@ dependencies {
   testImplementation("org.mockito:mockito-core:3.9.0")
   testImplementation("org.mockito:mockito-inline:3.9.0")
   testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
-  testImplementation("io.grpc:grpc-netty:1.39.0")
+  testImplementation("io.grpc:grpc-netty:1.40.1")
 }
