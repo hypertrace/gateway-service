1764861fda418bc2a61a7579df9eb2fffde51dc2
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-02-15 14:28:33 -0800

    chore: build cleanup (#118)

--- gateway-service-baseline-lib/build.gradle.kts
+++ gateway-service-baseline-lib/build.gradle.kts
@@ -13,14 +13,13 @@ dependencies {
   api(project(":gateway-service-api"))
 
   // Common utilities
-  implementation("org.apache.commons:commons-lang3:3.10")
-  implementation("org.apache.commons:commons-math:2.2")
-  implementation("com.google.protobuf:protobuf-java-util:3.17.3")
+  implementation("org.apache.commons:commons-lang3:3.12.0")
+  implementation("org.apache.commons:commons-math3:3.6.1")
+  implementation("com.google.protobuf:protobuf-java-util:3.19.4")
   constraints {
     implementation("com.google.code.gson:gson:2.8.9") {
       because("https://snyk.io/vuln/SNYK-JAVA-COMGOOGLECODEGSON-1730327")
     }
   }
-
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
--- gateway-service-impl/build.gradle.kts
+++ gateway-service-impl/build.gradle.kts
@@ -12,27 +12,27 @@ dependencies {
   api(project(":gateway-service-api"))
   api(project(":gateway-service-baseline-lib"))
 
-  implementation("org.hypertrace.core.query.service:query-service-client:0.7.0")
-  implementation("org.hypertrace.core.attribute.service:attribute-service-client:0.12.5")
-  implementation("org.hypertrace.entity.service:entity-service-client:0.8.11")
-  implementation("org.hypertrace.entity.service:entity-service-api:0.8.11")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.7.0")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.29")
+  implementation("org.hypertrace.core.query.service:query-service-client:0.7.1")
+  implementation("org.hypertrace.core.attribute.service:attribute-service-client:0.13.13")
+  implementation("org.hypertrace.entity.service:entity-service-client:0.8.23")
+  implementation("org.hypertrace.entity.service:entity-service-api:0.8.23")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.7.1")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.33")
 
   // Config
   implementation("com.typesafe:config:1.4.1")
 
   // Common utilities
-  implementation("org.apache.commons:commons-lang3:3.10")
-  implementation("org.apache.commons:commons-math:2.2")
-  implementation("com.google.protobuf:protobuf-java-util:3.17.3")
+  implementation("org.apache.commons:commons-lang3:3.12.0")
+  implementation("com.google.protobuf:protobuf-java-util:3.19.4")
+  implementation("com.google.guava:guava:30.1.1-jre")
 
-  implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.4")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.12.4")
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-  testImplementation("org.mockito:mockito-core:3.9.0")
-  testImplementation("org.mockito:mockito-inline:3.9.0")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+  testImplementation("org.mockito:mockito-core:4.3.1")
+  testImplementation("org.mockito:mockito-inline:4.3.1")
   testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
-  testImplementation("io.grpc:grpc-netty:1.43.1")
+  testImplementation("io.grpc:grpc-netty:1.44.0")
 }
