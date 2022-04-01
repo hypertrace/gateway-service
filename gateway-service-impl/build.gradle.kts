2856a11e9c1175c14c3198626cc3eacc9b66d3fc
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-04-01 06:17:04 -0700

    refactor: reduce grouped time series explorer limit (#124)
    * refactor: reduce grouped time series explorer limit
    
    * chore: snyk fix

--- gateway-service-impl/build.gradle.kts
+++ gateway-service-impl/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
   implementation("com.google.guava:guava:30.1.1-jre")
 
   implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
   testImplementation("org.mockito:mockito-core:4.3.1")
