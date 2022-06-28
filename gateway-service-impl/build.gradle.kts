d195385a47cd216995fe0ec4b1fa277d9708577b
GurtejSohi <gurtejsohi25@gmail.com>
2022-06-29 00:53:44 +0530

    feat: add CONTAINS_KEY_LIKE operator (#136)
    * feat: add CONTAINS_KEY_LIKE operator
    
    * fix: guava compilation issue

--- gateway-service-impl/build.gradle.kts
+++ gateway-service-impl/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
   api(project(":gateway-service-api"))
   api(project(":gateway-service-baseline-lib"))
 
-  implementation("org.hypertrace.core.query.service:query-service-client:0.7.1")
+  implementation("org.hypertrace.core.query.service:query-service-client:0.8.0")
   implementation("org.hypertrace.core.attribute.service:attribute-service-client:0.13.13")
   implementation("org.hypertrace.entity.service:entity-service-client:0.8.27")
   implementation("org.hypertrace.entity.service:entity-service-api:0.8.27")
@@ -25,7 +25,7 @@ dependencies {
   // Common utilities
   implementation("org.apache.commons:commons-lang3:3.12.0")
   implementation("com.google.protobuf:protobuf-java-util:3.19.4")
-  implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("com.google.guava:guava:31.0.1-jre")
 
   implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.2")
   implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
