04e51b8657254f3d1966075cbe8c4d99c8cd9508
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-06-29 15:44:59 +0200

    refactor: use service factory, grpc health check (#137)
    * refactor: use service factory, grpc health check
    
    * fix: update indentation on probe

---
+++ gateway-service-factory/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+  `java-library`
+}
+
+dependencies {
+  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.35")
+
+  implementation(project(":gateway-service-impl"))
+}
