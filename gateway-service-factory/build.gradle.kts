9dcff6f0b958087087f88c744c1350332792adce
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-07-06 12:53:04 +0200

    chore: update framework versio (#139)
    n

--- gateway-service-factory/build.gradle.kts
+++ gateway-service-factory/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.35")
+  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.36")
 
   implementation(project(":gateway-service-impl"))
 }
