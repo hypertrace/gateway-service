7542a2c9b8fd6ccf8e4a51c9ccf99d224ea13008
Aaron Steinfeld <aaron@traceable.ai>
2022-07-13 11:18:50 -0400

    style: spotless

--- gateway-service/src/main/java/org/hypertrace/gateway/service/GatewayServiceStarter.java
+++ gateway-service/src/main/java/org/hypertrace/gateway/service/GatewayServiceStarter.java
@@ -3,7 +3,6 @@ package org.hypertrace.gateway.service;
 import java.util.List;
 import org.hypertrace.core.serviceframework.config.ConfigClient;
 import org.hypertrace.core.serviceframework.grpc.GrpcPlatformServerDefinition;
-import org.hypertrace.core.serviceframework.grpc.GrpcPlatformServiceFactory;
 import org.hypertrace.core.serviceframework.grpc.StandAloneGrpcPlatformServiceContainer;
 
 public class GatewayServiceStarter extends StandAloneGrpcPlatformServiceContainer {
