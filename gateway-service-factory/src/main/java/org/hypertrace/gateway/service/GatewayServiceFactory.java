0cf5a250605a446a1c8918e068c770249096272c
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-08-03 21:30:46 -0700

    fix: restore removed config initialization (#142)

--- gateway-service-factory/src/main/java/org/hypertrace/gateway/service/GatewayServiceFactory.java
+++ gateway-service-factory/src/main/java/org/hypertrace/gateway/service/GatewayServiceFactory.java
@@ -4,12 +4,16 @@ import java.util.List;
 import org.hypertrace.core.serviceframework.grpc.GrpcPlatformService;
 import org.hypertrace.core.serviceframework.grpc.GrpcPlatformServiceFactory;
 import org.hypertrace.core.serviceframework.grpc.GrpcServiceContainerEnvironment;
+import org.hypertrace.gateway.service.entity.config.InteractionConfigs;
+import org.hypertrace.gateway.service.entity.config.TimestampConfigs;
 
 public class GatewayServiceFactory implements GrpcPlatformServiceFactory {
   private static final String SERVICE_NAME = "gateway-service";
 
   @Override
   public List<GrpcPlatformService> buildServices(GrpcServiceContainerEnvironment environment) {
+    InteractionConfigs.init(environment.getConfig(SERVICE_NAME));
+    TimestampConfigs.init(environment.getConfig(SERVICE_NAME));
     return List.of(
         new GrpcPlatformService(
             new GatewayServiceImpl(
