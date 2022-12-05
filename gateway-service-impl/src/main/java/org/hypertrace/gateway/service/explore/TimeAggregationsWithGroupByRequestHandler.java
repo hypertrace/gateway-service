11cd7cdb8c7d5bb8ee2177f7fa453757a0b25891
SJ <shubham@traceable.ai>
2022-12-05 18:53:02 +0530

    fix(explore): time aggregations group by

--- gateway-service-impl/src/main/java/org/hypertrace/gateway/service/explore/TimeAggregationsWithGroupByRequestHandler.java
+++ gateway-service-impl/src/main/java/org/hypertrace/gateway/service/explore/TimeAggregationsWithGroupByRequestHandler.java
@@ -1,7 +1,7 @@
 package org.hypertrace.gateway.service.explore;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.Set;
+import java.util.Optional;
+import org.hypertrace.core.attribute.service.v1.AttributeMetadata;
 import org.hypertrace.gateway.service.common.AttributeMetadataProvider;
 import org.hypertrace.gateway.service.common.util.ExpressionReader;
 import org.hypertrace.gateway.service.common.util.QueryServiceClient;
@@ -9,18 +9,25 @@ import org.hypertrace.gateway.service.v1.common.Expression;
 import org.hypertrace.gateway.service.v1.common.Filter;
 import org.hypertrace.gateway.service.v1.common.LiteralConstant;
 import org.hypertrace.gateway.service.v1.common.Operator;
+import org.hypertrace.gateway.service.v1.common.Row;
 import org.hypertrace.gateway.service.v1.common.Value;
 import org.hypertrace.gateway.service.v1.common.ValueType;
 import org.hypertrace.gateway.service.v1.explore.ExploreRequest;
 import org.hypertrace.gateway.service.v1.explore.ExploreResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TimeAggregationsWithGroupByRequestHandler implements IRequestHandler {
+  private static final Logger LOG =
+      LoggerFactory.getLogger(TimeAggregationsWithGroupByRequestHandler.class);
 
+  private final AttributeMetadataProvider attributeMetadataProvider;
   private final RequestHandler normalRequestHandler;
   private final TimeAggregationsRequestHandler timeAggregationsRequestHandler;
 
   TimeAggregationsWithGroupByRequestHandler(
       QueryServiceClient queryServiceClient, AttributeMetadataProvider attributeMetadataProvider) {
+    this.attributeMetadataProvider = attributeMetadataProvider;
     this.normalRequestHandler = new RequestHandler(queryServiceClient, attributeMetadataProvider);
     this.timeAggregationsRequestHandler =
         new TimeAggregationsRequestHandler(queryServiceClient, attributeMetadataProvider);
@@ -45,7 +52,8 @@ public class TimeAggregationsWithGroupByRequestHandler implements IRequestHandle
 
     // 2. Create a Time Aggregations request for the groups found in the request above. This will be
     // the actual query response
-    ExploreRequest timeAggregationsRequest = buildTimeAggregationsRequest(request, groupByResponse);
+    ExploreRequest timeAggregationsRequest =
+        buildTimeAggregationsRequest(requestContext, request, groupByResponse);
     ExploreRequestContext timeAggregationsRequestContext =
         new ExploreRequestContext(requestContext.getGrpcContext(), timeAggregationsRequest);
     ExploreResponse.Builder timeAggregationsResponse =
@@ -81,7 +89,9 @@ public class TimeAggregationsWithGroupByRequestHandler implements IRequestHandle
   }
 
   private ExploreRequest buildTimeAggregationsRequest(
-      ExploreRequest originalRequest, ExploreResponse.Builder groupByResponse) {
+      ExploreRequestContext originalRequestContext,
+      ExploreRequest originalRequest,
+      ExploreResponse.Builder groupByResponse) {
     ExploreRequest.Builder requestBuilder =
         ExploreRequest.newBuilder(originalRequest)
             .setIncludeRestGroup(
@@ -90,7 +100,8 @@ public class TimeAggregationsWithGroupByRequestHandler implements IRequestHandle
     // Create an "IN clause" filter to fetch time series only for the matching groups in the Group
     // By Response
     Filter.Builder inClauseFilter =
-        createInClauseFilterFromGroupByResults(originalRequest, groupByResponse);
+        createInClauseFilterFromGroupByResults(
+            originalRequestContext, originalRequest, groupByResponse);
     if (requestBuilder.hasFilter()
         && !(requestBuilder.getFilter().equals(Filter.getDefaultInstance()))) {
       requestBuilder.getFilterBuilder().addChildFilter(inClauseFilter);
@@ -102,42 +113,122 @@ public class TimeAggregationsWithGroupByRequestHandler implements IRequestHandle
   }
 
   private Filter.Builder createInClauseFilterFromGroupByResults(
-      ExploreRequest originalRequest, ExploreResponse.Builder groupByResponse) {
+      ExploreRequestContext originalRequestContext,
+      ExploreRequest originalRequest,
+      ExploreResponse.Builder groupByResponse) {
     Filter.Builder filterBuilder = Filter.newBuilder();
     filterBuilder.setOperator(Operator.AND);
     originalRequest
         .getGroupByList()
         .forEach(
             groupBy -> {
-              String groupByResultName =
-                  ExpressionReader.getSelectionResultName(groupBy).orElseThrow();
-              Set<String> inClauseValues = getInClauseValues(groupByResultName, groupByResponse);
-              filterBuilder.addChildFilter(createInClauseChildFilter(groupBy, inClauseValues));
+              Optional<Value> maybeInClauseValues =
+                  getInClauseValues(originalRequestContext, groupBy, groupByResponse);
+              maybeInClauseValues.ifPresent(
+                  inClauseValues ->
+                      filterBuilder.addChildFilter(
+                          createInClauseChildFilter(groupBy, inClauseValues)));
             });
 
     return filterBuilder;
   }
 
-  private Set<String> getInClauseValues(
-      String columnName, ExploreResponse.Builder exploreResponse) {
-    return exploreResponse.getRowBuilderList().stream()
-        .map(row -> row.getColumnsMap().get(columnName))
-        .map(Value::getString)
-        .collect(ImmutableSet.toImmutableSet());
+  private Optional<Value> getInClauseValues(
+      ExploreRequestContext originalRequestContext,
+      Expression groupBy,
+      ExploreResponse.Builder exploreResponse) {
+    String groupByResultName = ExpressionReader.getSelectionResultName(groupBy).orElseThrow();
+    Optional<AttributeMetadata> maybeGroupByAttributeMetadata =
+        attributeMetadataProvider.getAttributeMetadata(
+            originalRequestContext, originalRequestContext.getContext(), groupByResultName);
+
+    if (maybeGroupByAttributeMetadata.isEmpty()) {
+      return Optional.empty();
+    }
+
+    AttributeMetadata groupByAttributeMetadata = maybeGroupByAttributeMetadata.get();
+    // RHS value of in clause filter should always be an array to apply IN filter clause on groupBy
+    // expression
+    Value.Builder valueBuilder = Value.newBuilder();
+    switch (groupByAttributeMetadata.getValueKind()) {
+      case TYPE_STRING:
+      case TYPE_STRING_ARRAY:
+        valueBuilder.setValueType(ValueType.STRING_ARRAY);
+        break;
+      case TYPE_INT64:
+      case TYPE_INT64_ARRAY:
+        valueBuilder.setValueType(ValueType.LONG_ARRAY);
+        break;
+      case TYPE_DOUBLE:
+      case TYPE_DOUBLE_ARRAY:
+        valueBuilder.setValueType(ValueType.DOUBLE_ARRAY);
+        break;
+      case TYPE_BOOL:
+      case TYPE_BOOL_ARRAY:
+        valueBuilder.setValueType(ValueType.BOOLEAN_ARRAY);
+        break;
+      case TYPE_STRING_MAP:
+      case TYPE_TIMESTAMP:
+      case TYPE_BYTES:
+      case UNRECOGNIZED:
+      case KIND_UNDEFINED:
+        LOG.error(
+            "Group by isn't supported for attribute metadata {} of expression {}",
+            groupByAttributeMetadata,
+            groupBy);
+        break;
+    }
+
+    for (Row row : exploreResponse.getRowList()) {
+      Value groupByValue = row.getColumnsMap().get(groupByResultName);
+      switch (groupByValue.getValueType()) {
+        case STRING:
+          valueBuilder.addStringArray(groupByValue.getString());
+          continue;
+        case STRING_ARRAY:
+          valueBuilder.addAllStringArray(groupByValue.getStringArrayList());
+          continue;
+        case LONG:
+          valueBuilder.addLongArray(groupByValue.getLong());
+          continue;
+        case LONG_ARRAY:
+          valueBuilder.addAllLongArray(groupByValue.getLongArrayList());
+          continue;
+        case BOOL:
+          valueBuilder.addBooleanArray(groupByValue.getBoolean());
+          continue;
+        case BOOLEAN_ARRAY:
+          valueBuilder.addAllBooleanArray(groupByValue.getBooleanArrayList());
+          continue;
+        case DOUBLE:
+          valueBuilder.addDoubleArray(groupByValue.getDouble());
+          continue;
+        case DOUBLE_ARRAY:
+          valueBuilder.addAllDoubleArray(groupByValue.getDoubleArrayList());
+          continue;
+        case TIMESTAMP:
+        case STRING_MAP:
+        case UNRECOGNIZED:
+        case UNSET:
+          LOG.error(
+              "Unable to extract value for column {} and value {}", groupByResultName, groupByValue);
+      }
+    }
+
+    if (valueBuilder.getValueType() == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(valueBuilder.build());
   }
 
   private Filter.Builder createInClauseChildFilter(
-      Expression groupBySelectionExpression, Set<String> inClauseValues) {
+      Expression groupBySelectionExpression, Value inClauseValues) {
     return Filter.newBuilder()
         .setLhs(groupBySelectionExpression)
         .setOperator(Operator.IN)
         .setRhs(
             Expression.newBuilder()
-                .setLiteral(
-                    LiteralConstant.newBuilder()
-                        .setValue(
-                            Value.newBuilder()
-                                .setValueType(ValueType.STRING_ARRAY)
-                                .addAllStringArray(inClauseValues))));
+                .setLiteral(LiteralConstant.newBuilder().setValue(inClauseValues)));
   }
 }
