5336c3b870afe00c9b6196c82b069f3e06cef729
saxenakshitiz <kshitiz@traceable.ai>
2023-12-06 17:48:50 +0530

    minor changes

--- gateway-service-impl/src/main/java/org/hypertrace/gateway/service/common/converters/StringToAttributeKindConverter.java
+++ gateway-service-impl/src/main/java/org/hypertrace/gateway/service/common/converters/StringToAttributeKindConverter.java
@@ -24,8 +24,8 @@ public class StringToAttributeKindConverter extends ToAttributeKindConverter<Str
   private static final TypeReference<List<String>> LIST_TYPE_REFERENCE = new TypeReference<>() {};
   public static final StringToAttributeKindConverter INSTANCE =
       new StringToAttributeKindConverter();
-  private static final String ARRAY_NULL_VALUE = "null";
-  private static final List<String> LIST_NULL_VALUE = List.of("null");
+  private static final String STRING_ARRAY_NULL_VALUE = "null";
+  private static final List<String> LIST_WITH_NULL_VALUE = List.of("null");
   private final ObjectMapper objectMapper = new ObjectMapper();
 
   private StringToAttributeKindConverter() {}
@@ -95,10 +95,9 @@ public class StringToAttributeKindConverter extends ToAttributeKindConverter<Str
     }
 
     // handle special case when "null" string is returned as string array value(default value
-    // scenario)
-    // "null" is still a valid value, and should be converted to list with "null" value
-    if (ARRAY_NULL_VALUE.equals(jsonString)) {
-      return LIST_NULL_VALUE;
+    // scenario). "null" is still a valid value, and should be converted to list with "null" value
+    if (STRING_ARRAY_NULL_VALUE.equals(jsonString)) {
+      return LIST_WITH_NULL_VALUE;
     }
 
     // Check if the string is already in a list format.
