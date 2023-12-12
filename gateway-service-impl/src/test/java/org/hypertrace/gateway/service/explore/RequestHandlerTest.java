703388b807d5214c98b8b1b723d3b8b697c2e3c0
aman-bansal <bansalaman2905@gmail.com>
2023-12-12 13:47:58 +0530

    more robust test

--- gateway-service-impl/src/test/java/org/hypertrace/gateway/service/explore/RequestHandlerTest.java
+++ gateway-service-impl/src/test/java/org/hypertrace/gateway/service/explore/RequestHandlerTest.java
@@ -5,6 +5,8 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -13,11 +15,13 @@ import org.hypertrace.core.attribute.service.v1.AttributeMetadata;
 import org.hypertrace.core.attribute.service.v1.AttributeSource;
 import org.hypertrace.core.grpcutils.context.RequestContext;
 import org.hypertrace.core.query.service.api.ColumnMetadata;
+import org.hypertrace.core.query.service.api.QueryRequest;
 import org.hypertrace.core.query.service.api.ResultSetChunk;
 import org.hypertrace.core.query.service.api.ResultSetMetadata;
 import org.hypertrace.gateway.service.common.AttributeMetadataProvider;
 import org.hypertrace.gateway.service.common.datafetcher.EntityFetcherResponse;
 import org.hypertrace.gateway.service.common.datafetcher.QueryServiceEntityFetcher;
+import org.hypertrace.gateway.service.common.util.ExpressionReader;
 import org.hypertrace.gateway.service.common.util.QueryServiceClient;
 import org.hypertrace.gateway.service.entity.EntityKey;
 import org.hypertrace.gateway.service.entity.config.EntityIdColumnsConfigs;
@@ -374,13 +378,65 @@ public class RequestHandlerTest {
   }
 
   @Test
-  public void testExploreQueryWithEDSFilter() {
+  public void testExploreQueryWithEDSFilter() throws InvalidProtocolBufferException {
     ExploreRequest exploreRequest =
         ExploreRequest.newBuilder()
             .setContext("API")
             .setFilter(
                 Filter.newBuilder()
                     .setOperator(Operator.AND)
+                    .addChildFilter(
+                        Filter.newBuilder()
+                            .setOperator(Operator.AND)
+                            .addChildFilter(
+                                Filter.newBuilder()
+                                    .setOperator(Operator.AND)
+                                    .addChildFilter(
+                                        Filter.newBuilder()
+                                            .setLhs(
+                                                Expression.newBuilder()
+                                                    .setAttributeExpression(
+                                                        AttributeExpression.newBuilder()
+                                                            .setAttributeId("API.attributeId3")
+                                                            .build())
+                                                    .build())
+                                            .setOperator(Operator.EQ)
+                                            .setRhs(
+                                                Expression.newBuilder()
+                                                    .setLiteral(
+                                                        LiteralConstant.newBuilder()
+                                                            .setValue(
+                                                                Value.newBuilder()
+                                                                    .setValueType(ValueType.STRING)
+                                                                    .setString("value")
+                                                                    .build())
+                                                            .build())
+                                                    .build())
+                                            .build())
+                                    .addChildFilter(
+                                        Filter.newBuilder()
+                                            .setLhs(
+                                                Expression.newBuilder()
+                                                    .setAttributeExpression(
+                                                        AttributeExpression.newBuilder()
+                                                            .setAttributeId("API.attributeId4")
+                                                            .build())
+                                                    .build())
+                                            .setOperator(Operator.EQ)
+                                            .setRhs(
+                                                Expression.newBuilder()
+                                                    .setLiteral(
+                                                        LiteralConstant.newBuilder()
+                                                            .setValue(
+                                                                Value.newBuilder()
+                                                                    .setValueType(ValueType.STRING)
+                                                                    .setString("value")
+                                                                    .build())
+                                                            .build())
+                                                    .build())
+                                            .build())
+                                    .build())
+                            .build())
                     .addChildFilter(
                         Filter.newBuilder()
                             .setLhs(
@@ -458,6 +514,23 @@ public class RequestHandlerTest {
                                                     .setLong(30))))))
             .build();
 
+    Map<String, AttributeMetadata> attributeMap =
+        Map.of(
+            "API.attributeId1",
+            AttributeMetadata.newBuilder()
+                .addAllSources(List.of(AttributeSource.EDS, AttributeSource.QS))
+                .build(),
+            "API.attributeId2",
+            AttributeMetadata.newBuilder().addSources(AttributeSource.QS).build(),
+            "API.attributeId3",
+            AttributeMetadata.newBuilder()
+                .addAllSources(List.of(AttributeSource.EDS, AttributeSource.QS))
+                .build(),
+            "API.attributeId4",
+            AttributeMetadata.newBuilder().addSources(AttributeSource.EDS).build());
+    Filter edsFilter =
+        buildFilter(exploreRequest.getFilter(), AttributeSource.EDS, attributeMap).get();
+
     AttributeMetadataProvider attributeMetadataProvider = mock(AttributeMetadataProvider.class);
     QueryServiceEntityFetcher queryServiceEntityFetcher = mock(QueryServiceEntityFetcher.class);
     EntityServiceEntityFetcher entityServiceEntityFetcher = mock(EntityServiceEntityFetcher.class);
@@ -474,22 +547,11 @@ public class RequestHandlerTest {
         new ExploreRequestContext(RequestContext.forTenantId("tenantId"), exploreRequest);
 
     when(entityIdColumnsConfigs.getIdKey("API")).thenReturn(Optional.of("entityId"));
-    when(attributeMetadataProvider.getAttributesMetadata(any(), any()))
-        .thenReturn(
-            Map.of(
-                "API.attributeId1",
-                    AttributeMetadata.newBuilder()
-                        .addAllSources(List.of(AttributeSource.EDS, AttributeSource.QS))
-                        .build(),
-                "API.attributeId2",
-                    AttributeMetadata.newBuilder().addSources(AttributeSource.EDS).build()));
-
+    when(attributeMetadataProvider.getAttributesMetadata(any(), any())).thenReturn(attributeMap);
     when(attributeMetadataProvider.getAttributeMetadata(any(), any(), eq("entityId")))
         .thenReturn(Optional.of(AttributeMetadata.newBuilder().setId("API.id").build()));
-
     when(attributeMetadataProvider.getAttributeMetadata(any(), any(), any()))
         .thenReturn(Optional.of(AttributeMetadata.newBuilder().setId("API.timestampId").build()));
-
     when(queryServiceEntityFetcher.getEntities(any(), any()))
         .thenReturn(
             new EntityFetcherResponse(
@@ -500,10 +562,20 @@ public class RequestHandlerTest {
                     Entity.newBuilder().setEntityType("API").setId("entityId2"),
                     EntityKey.from("entityId3"),
                     Entity.newBuilder().setEntityType("API").setId("entityId3"))));
-
     when(entityServiceEntityFetcher.getResults(
             any(),
-            eq(ExploreRequest.newBuilder().setFilter(exploreRequest.getFilter()).build()),
+            eq(
+                ExploreRequest.newBuilder()
+                    .setContext("API")
+                    .setFilter(edsFilter)
+                    .addGroupBy(
+                        Expression.newBuilder()
+                            .setAttributeExpression(
+                                AttributeExpression.newBuilder()
+                                    .setAttributeId("API.timestampId")
+                                    .build())
+                            .build())
+                    .build()),
             eq(Set.of("entityId1", "entityId2", "entityId3"))))
         .thenReturn(
             List.of(
@@ -514,7 +586,7 @@ public class RequestHandlerTest {
                     .putColumns("entityId", Value.newBuilder().setString("entityId2").build())
                     .build()));
 
-    when(queryServiceClient.executeQuery(any(), any()))
+    when(queryServiceClient.executeQuery(any(), eq(getQueryRequest())))
         .thenReturn(
             List.of(
                     ResultSetChunk.newBuilder()
@@ -556,5 +628,189 @@ public class RequestHandlerTest {
     Builder responseBuilder =
         requestHandler.handleRequest(newExploreRequestContext, exploreRequest);
     Assertions.assertEquals(2, responseBuilder.getRowList().size());
+  }
+
+  private QueryRequest getQueryRequest() throws InvalidProtocolBufferException {
+    QueryRequest.Builder requestBuilder = QueryRequest.newBuilder();
+    JsonFormat.parser()
+        .ignoringUnknownFields()
+        .merge(
+            "{\n"
+                + "  \"filter\": {\n"
+                + "    \"childFilter\": [{\n"
+                + "      \"childFilter\": [{\n"
+                + "        \"lhs\": {\n"
+                + "          \"attributeExpression\": {\n"
+                + "            \"attributeId\": \"API.timestampId\"\n"
+                + "          }\n"
+                + "        },\n"
+                + "        \"operator\": \"GE\",\n"
+                + "        \"rhs\": {\n"
+                + "          \"literal\": {\n"
+                + "            \"value\": {\n"
+                + "              \"valueType\": \"LONG\"\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }, {\n"
+                + "        \"lhs\": {\n"
+                + "          \"attributeExpression\": {\n"
+                + "            \"attributeId\": \"API.timestampId\"\n"
+                + "          }\n"
+                + "        },\n"
+                + "        \"operator\": \"LT\",\n"
+                + "        \"rhs\": {\n"
+                + "          \"literal\": {\n"
+                + "            \"value\": {\n"
+                + "              \"valueType\": \"LONG\"\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }]\n"
+                + "    }, {\n"
+                + "      \"childFilter\": [{\n"
+                + "        \"childFilter\": [{\n"
+                + "          \"childFilter\": [{\n"
+                + "            \"lhs\": {\n"
+                + "              \"attributeExpression\": {\n"
+                + "                \"attributeId\": \"API.attributeId3\"\n"
+                + "              }\n"
+                + "            },\n"
+                + "            \"operator\": \"EQ\",\n"
+                + "            \"rhs\": {\n"
+                + "              \"literal\": {\n"
+                + "                \"value\": {\n"
+                + "                  \"string\": \"value\"\n"
+                + "                }\n"
+                + "              }\n"
+                + "            }\n"
+                + "          }]\n"
+                + "        }]\n"
+                + "      }, {\n"
+                + "        \"lhs\": {\n"
+                + "          \"attributeExpression\": {\n"
+                + "            \"attributeId\": \"API.attributeId1\"\n"
+                + "          }\n"
+                + "        },\n"
+                + "        \"operator\": \"EQ\",\n"
+                + "        \"rhs\": {\n"
+                + "          \"literal\": {\n"
+                + "            \"value\": {\n"
+                + "              \"string\": \"value\"\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }, {\n"
+                + "        \"lhs\": {\n"
+                + "          \"attributeExpression\": {\n"
+                + "            \"attributeId\": \"API.attributeId2\"\n"
+                + "          }\n"
+                + "        },\n"
+                + "        \"operator\": \"EQ\",\n"
+                + "        \"rhs\": {\n"
+                + "          \"literal\": {\n"
+                + "            \"value\": {\n"
+                + "              \"string\": \"value\"\n"
+                + "            }\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }]\n"
+                + "    }, {\n"
+                + "      \"lhs\": {\n"
+                + "        \"attributeExpression\": {\n"
+                + "          \"attributeId\": \"API.timestampId\"\n"
+                + "        }\n"
+                + "      },\n"
+                + "      \"operator\": \"IN\",\n"
+                + "      \"rhs\": {\n"
+                + "        \"literal\": {\n"
+                + "          \"value\": {\n"
+                + "            \"valueType\": \"STRING_ARRAY\",\n"
+                + "            \"stringArray\": [\"entityId1\", \"entityId2\"]\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }\n"
+                + "    }]\n"
+                + "  },\n"
+                + "  \"selection\": [{\n"
+                + "    \"function\": {\n"
+                + "      \"functionName\": \"AVG\",\n"
+                + "      \"arguments\": [{\n"
+                + "        \"attributeExpression\": {\n"
+                + "          \"attributeId\": \"Api.Trace.metrics.duration_millis\"\n"
+                + "        }\n"
+                + "      }],\n"
+                + "      \"alias\": \"AVG_Duration\"\n"
+                + "    }\n"
+                + "  }, {\n"
+                + "    \"function\": {\n"
+                + "      \"functionName\": \"AVGRATE\",\n"
+                + "      \"arguments\": [{\n"
+                + "        \"attributeExpression\": {\n"
+                + "          \"attributeId\": \"Api.Trace.metrics.duration_millis\"\n"
+                + "        }\n"
+                + "      }, {\n"
+                + "        \"literal\": {\n"
+                + "          \"value\": {\n"
+                + "            \"string\": \"PT30S\"\n"
+                + "          }\n"
+                + "        }\n"
+                + "      }],\n"
+                + "      \"alias\": \"RATE_Duration\"\n"
+                + "    }\n"
+                + "  }]\n"
+                + "}",
+            requestBuilder);
+
+    return requestBuilder.build();
+  }
+
+  private Optional<org.hypertrace.gateway.service.v1.common.Filter> buildFilter(
+      org.hypertrace.gateway.service.v1.common.Filter filter,
+      AttributeSource source,
+      Map<String, AttributeMetadata> attributeMetadataMap) {
+    if (filter.equals(org.hypertrace.gateway.service.v1.common.Filter.getDefaultInstance())) {
+      return Optional.empty();
+    }
+
+    org.hypertrace.gateway.service.v1.common.Operator operator = filter.getOperator();
+    switch (operator) {
+      case UNDEFINED:
+        return Optional.empty();
+      case AND:
+      case OR:
+        return buildCompositeFilter(filter, source, operator, attributeMetadataMap);
+      default:
+        List<AttributeSource> availableSources =
+            attributeMetadataMap
+                .get(
+                    ExpressionReader.getAttributeIdFromAttributeSelection(filter.getLhs())
+                        .orElseThrow())
+                .getSourcesList();
+        return availableSources.contains(source)
+            ? Optional.of(
+                org.hypertrace.gateway.service.v1.common.Filter.newBuilder(filter).build())
+            : Optional.empty();
+    }
+  }
+
+  private Optional<org.hypertrace.gateway.service.v1.common.Filter> buildCompositeFilter(
+      org.hypertrace.gateway.service.v1.common.Filter filter,
+      AttributeSource source,
+      org.hypertrace.gateway.service.v1.common.Operator operator,
+      Map<String, AttributeMetadata> attributeMetadataMap) {
+    org.hypertrace.gateway.service.v1.common.Filter.Builder filterBuilder =
+        org.hypertrace.gateway.service.v1.common.Filter.newBuilder();
+    for (org.hypertrace.gateway.service.v1.common.Filter childFilter :
+        filter.getChildFilterList()) {
+      buildFilter(childFilter, source, attributeMetadataMap)
+          .ifPresent(filterBuilder::addChildFilter);
+    }
+    if (filterBuilder.getChildFilterCount() > 0) {
+      filterBuilder.setOperator(operator);
+      return Optional.of(filterBuilder.build());
+    } else {
+      return Optional.empty();
+    }
   }
 }
