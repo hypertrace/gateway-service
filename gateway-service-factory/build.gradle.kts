77592de082855d37edb4fe1efc8312476d83413b
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2024-04-16 21:33:33 +0530

    update service framework version (#198)

--- gateway-service-factory/build.gradle.kts
+++ gateway-service-factory/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.64")
+  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.71")
 
   implementation(project(":gateway-service-impl"))
 }
